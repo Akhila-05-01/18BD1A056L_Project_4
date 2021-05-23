cf107f80a7078445a4aa6f5dc6baf9f0a96be8db
Akhila-05-01 <72184331+Akhila-05-01@users.noreply.github.com>
2021-05-23 03:24:27 -0230

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import requests
+from flask import Flask, render_template, request
+from twilio.rest import Client
+import requests
+
+account_sid='ACa11770075611486e5a18f0b9a8ec456c'
+auth_token='66c1591c023cdb5b8d1c8231c2cab273'
+
+client=Client(account_sid,auth_token)
+app=Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def registration_form():
+    return render_template('login_page.html')
+
+@app.route('/login_page', methods=['POST', 'GET'])
+def login_registration_dtls():
+    first_name = request.form['fname']
+    last_name = request.form['lname']
+    email_id = request.form['email']
+    source_st = request.form['source_state']
+    source_dt = request.form['source']
+    destination_st = request.form['dest_state']
+    destination_dt = request.form['destination']
+    phone_number = request.form['phone']
+    date = request.form['trip']
+    full_name = first_name + "." + last_name
+    r = requests.get('https://api.covid19india.org/v4/data.json')
+    json_data = r.json()
+    cnt = json_data[destination_st]['districts'][destination_dt]['total']['confirmed']
+    pop = json_data[destination_st]['districts'][destination_dt]['meta']['population']
+    travel_pass = ((cnt / pop) * 100)
+    if travel_pass < 30 and request.method == 'POST':
+        status = 'CONFIRMED'
+        client.messages.create(to="whatsapp:+919381211696",from_="whatsapp:+14155238886",
+                               body = "Hello" + " " + full_name + " " + "Your Travel Form" + " " + source_dt + " " + "To" + " " +
+                               destination_dt + " " +"Has" + " " + status + " " + "On" + " " + date + " " + ",Safe Journey")
+        return render_template('user_registration_dtls.html', var=full_name, var1=email_id,
+                               var3=source_st, var4=source_dt, var5=destination_st, var6=destination_dt,
+                               var7=phone_number, var8=date, var9=status)
+    else:
+        status = 'NOT CONFIRMED'
+        client.messages.create(to="whatsapp:+919381211696",from_="whatsapp:+14155238886",
+                               body = "Hello" + " " + full_name + " " + "Your Travel Form" + " " + source_dt + " " + "To" + " " +
+                               destination_dt + " " +
+                               "Has" + " " + status + " " + "On" + " " + date + " " + ",Safe Journey")
+        return render_template('user_registration_dtls.html', var=full_name, var1=email_id,
+                               var3=source_st, var4=source_dt, var5=destination_st, var6=destination_dt,
+                               var7=phone_number, var8=date, var9=status)
+
+
+if __name__ ==  "__main__":
+    app.run(port=9001, debug=True)
+
+
+
+
